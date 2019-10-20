1e8c3b1843145c738553440d0a259036464ac58f
AutismThrow <36837780+AutismThrow@users.noreply.github.com>
2019-10-20 20:25:17 +0800

    git init

---
+++ src/test/java/com/study/example/ConcurrentProgrammingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.example;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrentProgrammingDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
